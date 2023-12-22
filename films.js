adfa6dff0c4b213e6596db64a2385870bb906008
MentseurFares <mentseur.fares@hotmail.fr>
2023-12-22 15:02:33 +0100

    Fetch picture from TheMovieDB API done

--- films.js
+++ films.js
@@ -1,41 +1,48 @@
 const urlAppFilms = 'http://localhost:8080/films/all';
-
+const API_KEY = '8c876ad71559ac44edf7af86b9d77927'
 
 async function getFilms(pageNumber, pageSize) {
     let datas = await fetch(`${urlAppFilms}?page=${pageNumber}&size=${pageSize}`)
-        .then((response) => {return response.json()})
+        .then((response) => { return response.json() })
         .catch(error => console.log(error))
-    
-    return datas
 
+    return datas
 }
 
 async function getPictures(imdbId) {
-    const imdbUrl = `https://api.themoviedb.org/3/find/${imdbId}?external_source=imdb_id`;
-    let datas = await fetch(imdbUrl, {
-        method: 'GET',
-        withCredentials: true,
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods':'GET,POST,PATCH,OPTIONS',
-            'Authorization': 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Yzg3NmFkNzE1NTlhYzQ0ZWRmN2FmODZiOWQ3NzkyNyIsInN1YiI6IjY1ODJkOWE3MDgzNTQ3NDQ2ZjNlODM2NiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.OXEejaV2yeDOEPnDrrpgz3seL0zwQdNYQti6UZt53hA',
-            'X-Auth-Token': '8c876ad71559ac44edf7af86b9d77927',
-            'accept': 'application/json'
-        }
-    })
+    const imdbUrl = `https://api.themoviedb.org/3/find/${imdbId}?external_source=imdb_id&api_key=${API_KEY}`;
+    let datas = await fetch(imdbUrl)
         .then(response => response.json())
-        .then(data => console.log(data.person_results[0].known_for))
+        .then(data => {
+            if(data.movie_results.length != 0){
+                return data.movie_results[0].poster_path
+            }else if(data.person_results.length != 0){
+                return data.person_results[0].poster_path
+            }else if(data.tv_results.length != 0){
+                return data.tv_results[0].poster_path
+            }else if(data.tv_episode_results.length != 0){
+                return data.tv_episode_results[0].poster_path
+            }else if(data.tv_season_results.length != 0){
+                return data.tv_season_results[0].poster_path
+            }else {
+                return ''
+            }
+
+
+        })
         .catch(error => console.log(error))
-    
-    return datas
+
+    return `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datas}`
 }
 
 let filmData = [];
 const filmDisplay = async () => {
     filmData = await getFilms(1, 2);
     filmData.content.forEach(async (film) => {
-         let picture = await getPictures(film.referenceNumber);
-        console.log(getPictures);
+        console.log(film.referenceNumber);
+        let picture = await getPictures(film.referenceNumber);
+        console.log(picture);
+        
     });
     console.log(filmData);
 
