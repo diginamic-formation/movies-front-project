3d81d951043277d21cfd1e5cbe961d9b4834bb0e
Garmi <fallgarmi83@yahoo.fr>
2023-12-26 17:15:01 +0100

    Pagination

--- films.js
+++ films.js
@@ -1,14 +1,21 @@
+
 const urlAppFilms = 'http://localhost:8080/films/all';
-const API_KEY = '8c876ad71559ac44edf7af86b9d77927'
+const API_KEY = '8c876ad71559ac44edf7af86b9d77927';
 
-async function getFilms(pageNumber, pageSize) {
-    let datas = await fetch(`${urlAppFilms}?page=${pageNumber}&size=${pageSize}`)
-        .then((response) => { return response.json() })
-        .catch(error => console.log(error))
+const pageSize = 12;
+let totalPageCount = 332; // Nombre total de pages
+let currentPage = 0;
 
-    return datas
+async function getFilms(pageNumber) {
+    try {
+        const response = await fetch(`${urlAppFilms}?page=${pageNumber}&size=${pageSize}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.error('Error fetching films:', error);
+        throw error;
+    }
 }
-
 
 async function getPictures(imdbId) {
     const imdbUrl = `https://api.themoviedb.org/3/find/${imdbId}?external_source=imdb_id&api_key=${API_KEY}`;
@@ -36,25 +43,44 @@ async function getPictures(imdbId) {
     return `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datas}`
 }
 
-let filmData = [];
-const filmDisplay = async () => {
-    filmData = await getFilms(1, 8);
-    for(film of filmData.content){
-        let picture = await getPictures(film.referenceNumber);
-        film.picture = picture
+async function filmDisplay(pageNumber) {
+    try {
+        if (pageNumber < 0 || pageNumber > totalPageCount) {
+            return; // Ne rien faire si la page demandée est en dehors des limites
+        }
+
+        const filmData = await getFilms(pageNumber);
+        for (const film of filmData.content) {
+            const picture = await getPictures(film.referenceNumber);
+            film.picture = picture;
+        }
+console.log(filmData);
+        document.querySelector(".films").innerHTML = filmData.content.map((film) => `
+            <article class="col">
+                <div class="card">
+                    <img class="card-img-top" src=${film.picture} alt="image du film ${film.title}" />
+                    <div class="card-body">
+                        <h5 class="card-title text-truncate">${film.title}</h5>
+                        <p class="card-text text-truncate">${film.genres}</p>
+                    </div>
+                    <div class="card-footer">
+                        <a href="cart-film.html" class="btn btn-outline-dark detail-film" id=${film.id}>Voir</a>
+                    </div>
+                </div>
+            </article>
+        `).join("");
+
+        currentPage = pageNumber;
+        return filmData;
+    } catch (error) {
+        console.error('Error displaying films:', error);
+        throw error;
     }
-
-    document.querySelector(".films").innerHTML = filmData.content.map((film) => `
-    <article class="col">
-    <div class="card">
-    <img class="card-img-top" src=${film.picture} />
-    <div class="card-body">
-    <h5 class="card-title text-truncate">${film.title}</h5>
-    <p class="card-text text-truncate">${film.genres}</p>
-    </div>
-    </div>
-    </article>
-    `).join("")
-
 }
-filmDisplay();
+
+document.getElementById("prev").addEventListener("click", () => filmDisplay(currentPage - 1));
+document.getElementById("next").addEventListener("click", () => filmDisplay(currentPage + 1));
+
+// Initial display
+filmDisplay(currentPage);
+
