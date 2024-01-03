d1c940fc6d6f6feaa770184c048d4fdce8649ce6
rijarandria <rijandrisolo@gmail.com>
2024-01-03 13:57:34 +0100

    genre.html : ajout, modif, suppression ok. Réglage affichage modif à faire, affichage film à faire. données ok

---
+++ crudGenre.js
@@ -0,0 +1,144 @@
+
+const urlAppGenre = 'http://localhost:8080/genres'
+const urlAppGenreId = 'http://localhost:8080/genres/'
+const urlAppGenreInsert = 'http://localhost:8080/genres/insertGenre'
+
+/**getGenreById
+ * non utilisé
+ * @param {*} id 
+ * @returns 
+ */
+async function getGenreById(id){
+    let datas = await fetch(`${urlAppGenreId}{id}`)
+    .then((response) => {return response.json()})
+    .catch(error => console.log(error))
+return datas
+}
+
+/**
+ * getGenres
+ * affichage des genres de films
+ * 
+ */
+
+async function getGenres() {
+    let datas = await fetch(`${urlAppGenre}`)
+        .then((response) => {return response.json()})
+        .then(data =>{
+             genres = data
+            
+          
+        })
+        .catch(error => console.log(error))
+    
+    return datas
+
+}
+/**
+ * getFilmsByidGenre 
+ * affichage des films par genre (id)
+ * @param idGenr id genre récupéré
+ * @returns liste de films
+ */
+async function getFilmsByidGenre(idGenr) {
+    console.log(`${idGenr}`);
+    let datas = await fetch(`http://localhost:8080/genres/${idGenr}/films`)
+        .then((response) => {return response.json()})
+        .then(data =>{
+             films = data
+              console.log(films);
+          
+        })
+        .catch(error => console.log(error))
+    
+    return datas
+
+}
+
+
+/**
+ * addGenre
+ * permet d'ajouter des nouveaux genres
+ * 
+ */
+function addGenre() {
+
+
+    //alert("insert")
+    let newGenre = document.querySelector("#newGenre").value
+    if (newGenre) {
+
+        const result = fetch("http://localhost:8080/genres/insertGenre",
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "PUT",
+                body: JSON.stringify({ nameGenre: newGenre })
+            }
+        )
+            .then(res => document.location.href="http://127.0.0.1:5500/genre.html");
+    }
+    else {
+        alert("Saisie vide")
+    }
+}
+
+/**
+ * updateGenre
+ * mise à jour et modification dans la base d'un genre
+ */
+
+function updateGenre() {
+    let updatedGenre = document.querySelector("#newValGenre").value
+    let idGenre = document.querySelector("#idGenre").value
+    let id = parseInt(idGenre)
+    if (updatedGenre) {
+       
+        const result = fetch(`http://localhost:8080/genres/update/${id}`,
+            {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "POST",
+                body: JSON.stringify({ nameGenre: updatedGenre })
+            }
+        )
+            .then(res => document.location.href="http://127.0.0.1:5500/genre.html");
+            
+    }
+    else {
+        alert("Modification non effectuée")
+    }
+
+
+}
+
+/**
+ * deleteGenre
+ * permet de supprimer un genre
+ * seul un genre qui n'a pas de films associés pourra être supprimé
+ * 
+ * @param {*} idG id du genre à supprimer
+ */
+function deleteGenre(idG){
+    console.log(idG)
+   
+   
+   const result = fetch(`http://localhost:8080/genres/delete/${idG}`,
+           {
+                headers: {
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json'
+                },
+                method: "DELETE",
+                //body: JSON.stringify({ nameGenre: updatedGenre })
+            }
+        )
+            .then(res =>document.location.href="http://127.0.0.1:5500/genre.html");
+   
+}
+
+
--- fichierindex.js
+++ fichierindex.js
@@ -1,34 +1,12 @@
 
-const urlAppGenre = 'http://localhost:8080/genres'
-const urlAppGenreId = 'http://localhost:8080/genres/'
-const urlAppGenreInsert = 'http://localhost:8080/genres/insertGenre'
 
-let vNom1 ="";
-let filmData = [];
+/**
+ * rechercher films par nom sur navbar
+ * non utilisé
+ * 
+ */
 
-
-async function getGenreById(id){
-    let datas = await fetch(`${urlAppGenreId}{id}`)
-    .then((response) => {return response.json()})
-    .catch(error => console.log(error))
-return datas
-}
-
-async function getGenres() {
-    let datas = await fetch(`${urlAppGenre}`)
-        .then((response) => {return response.json()})
-        .then(data =>{
-             genres = data
-            
-          
-        })
-        .catch(error => console.log(error))
-    
-    return datas
-
-}
-
-/*async function choice(){
+async function choice(){
     
     const vChoix = document.querySelector("#choix").value;
     vNom = document.querySelector("#name").value;
@@ -41,83 +19,112 @@ async function getGenres() {
        // await personDisplay(vNom)
     }
 
-}*/
+}
+
+/**
+ * rendre visible les champs de saisie ajout nouveau genre
+ */
+
 function saisie(){
     
     champs.style.visibility = "visible"
 }
 
-function validGenre(){
-    
-   
-    //alert("insert")
-    let newGenre = document.querySelector("#newGenre").value
-    newGenre = JSON.stringify({nameGenre: newGenre})
+/**modifGenre
+ * permet de saisir la modification du genre
+ * @param {*} id id du genre
+ * @param {*} nameGenre le genre
+ */
 
-    console.log(typeof(newGenre));
-        console.log(newGenre);
-    if (newGenre){
-       
-       const result =  fetch("http://localhost:8080/genres/insertGenre",
-                                {
-                                    method: "PUT",
-                                    body:{ 
-                                             newGenre
-                                    }
-                                }
-                               
-                            )
-                            .then( res => console.log(res));                       
-     
-    }
-    else{
-        alert("Saisie vide")
-    }
+function modifGenre(id, nameGenre){
+console.log(id);
+console.log(nameGenre);
 
-}
-function delGenre(){
+   let modif = document.querySelector("#modifmodal") 
+   modifmodal.innerHTML = (`
+        
+        <input type="hidden" id="idGenre" value ="${id}"></input>
+        <input type="text" id = "newValGenre" value ="${nameGenre.name}"></input>
+   `)
    
-   
-}
-function modifGenre(){
     
-   
+   }
+/**genreDisp
+ * permet d'afficher les genres dans genre.html
+ * 
+ */
+const genreDisp = async () => {
     
-}
-
- const genreDisp = async () => {
+    const genreData = await getGenres();
+    let element =  document.querySelector("#genre") 
     
-        const genreData = await getGenres();
-        let element =  document.querySelector("#genre")
+    
+    genres.forEach(genre => {
+    
+    
+    // console.log(genre)
         
-        genres.forEach(genre => {
-        
-        
-        // console.log(genre)
-            
-            element.innerHTML +=  (`
-            <div >
-        
-                <article class="col">
-                    <div class="card shadow p-3 mb-5 bg-body-tertiary rounded bg-primary-subtle" style="width: 22rem;">
-                        <div class="card-body ">
-                            <h5 class="card-title text-truncate">${genre.nameGenre}</h5>
-                            
-                        </div>
-                        <div class="card-footer">
-                            <input id="essai" name = "essai" value=${genre.id}></input>
-                            <button class="btn btn-outline-dark " >Les films</button>
-                            <button type ="submit" class="btn btn-outline-primary "  onclick = "modifGenre() ">Modifier </button>
-                            <button type ="submit" class="btn btn-outline-danger "  onclick = "delGenre()">Supprimer </button>
-                        </div>
+        element.innerHTML +=  (`
+        <div >
+    
+            <article class="col">
+                <div class="card shadow p-3 mb-5 bg-body-tertiary rounded bg-primary-subtle" style="width: 22rem;">
+                    <div class="card-body ">
+                        <h5 class="card-title text-truncate" name="titre">${genre.nameGenre}</h5>
+                        <input type="hidden" id="${genre.nameGenre}" name = "${genre.nameGenre}" value ="${genre.nameGenre}"></input>
                     </div>
-                </article>
-            </div>
-            `)
-            }); 
-   
-    
-     
+                    <div class="card-footer">
+                       
+                        <button class="btn btn-outline-dark " onclick = "getFilmsByidGenre(${genre.id})" >Les films</button>
+                        <button "button" class="btn btn-primary" data-toggle="modal" data-target="#Modal" onclick = "modifGenre(${genre.id}, ${genre.nameGenre}) ">Modifier </button>
+                        <button type ="submit" class="btn btn-outline-danger "  onclick = "deleteGenre(${genre.id})">Supprimer </button>
+                    </div>
+                </div>
+            </article>
+        </div>
+        `)
+
+        }); 
+
+
+ 
 } 
+const filmDisp = async () => {
+    
+    const filmData = await getFilmsByidGenre(idGenr);
+    let element =  document.querySelector("#genre") 
+    
+    
+    genres.forEach(genre => {
+    
+    
+    // console.log(genre)
+        
+        element.innerHTML +=  (`
+        <div >
+    
+            <article class="col">
+                <div class="card shadow p-3 mb-5 bg-body-tertiary rounded bg-primary-subtle" style="width: 22rem;">
+                    <div class="card-body ">
+                        <h5 class="card-title text-truncate" name="titre">${genre.nameGenre}</h5>
+                        <input type="hidden" id="${genre.nameGenre}" name = "${genre.nameGenre}" value ="${genre.nameGenre}"></input>
+                    </div>
+                    <div class="card-footer">
+                       
+                        <button class="btn btn-outline-dark " onclick = "getFilmsByidGenre(${genre.id})" >Les films</button>
+                        <button "button" class="btn btn-primary" data-toggle="modal" data-target="#Modal" onclick = "modifGenre(${genre.id}, ${genre.nameGenre}) ">Modifier </button>
+                        <button type ="submit" class="btn btn-outline-danger "  onclick = "deleteGenre(${genre.id})">Supprimer </button>
+                    </div>
+                </div>
+            </article>
+        </div>
+        `)
+
+        }); 
+
+
+ 
+} 
+
 
 
