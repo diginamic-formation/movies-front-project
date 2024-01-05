bdb547bc118f3645722dcf35096b27a4e7a709a7
Garmi <fallgarmi83@yahoo.fr>
2024-01-05 15:18:41 +0100

    Ajout de fonctionnalités

--- cart-film.js
+++ cart-film.js
@@ -1,8 +1,15 @@
 // Extraction de la chaîne de requête de l'URL, puis suppression du caractère "?" pour obtenir l'ID du film
 const cartFilm = window.location.search.split("?").join("");
-const urlAppFilms = 'http://localhost:8080/films/all';
+
+const urlAppFilm = 'http://localhost:8080/films/'
+const urlAppFilms = 'http://localhost:8080/films';
 const API_KEY = '8c876ad71559ac44edf7af86b9d77927';
 
+/**
+ * Effectue une requête asynchrone pour récupérer les détails d'un film spécifique en utilisant son identifiant.
+ * @param {string} cartFilm - L'identifiant du film à récupérer.
+ * @returns {Promise<Object>} - Une promesse résolue avec les données du film si la requête est réussie, sinon une promesse rejetée avec l'erreur.
+ */
 async function getFilmById(cartFilm){
     let datas = await fetch(`${urlAppFilms}/${cartFilm}`)
     .then((response) => {return response.json()})
@@ -10,84 +17,226 @@ async function getFilmById(cartFilm){
     return datas
    
 }
-
+/**
+ * Effectue une requête asynchrone pour récupérer les rôles associés à un film spécifique.
+ * @returns {Promise<Array>} - Une promesse résolue avec un tableau de rôles si la requête est réussie, sinon une promesse rejetée avec l'erreur.
+ */
 async function getRoleByFilm(){
     let datas = await fetch(`${urlAppFilms}/${cartFilm}/roles`)
     .then((response) => {return response.json()})
     .catch(error => console.log(error))
     return datas
 }
-
-async function getPictures(imdbId) {
-    const imdbUrl = `https://api.themoviedb.org/3/find/${imdbId}?external_source=imdb_id&api_key=${API_KEY}`;
-    let datas = await fetch(imdbUrl)
-        .then(response => response.json())
-        .then(data => {
-            if (data.movie_results.length != 0) {
-                return data.movie_results[0].poster_path
-            } else if (data.person_results.length != 0) {
-                return data.person_results[0].poster_path
-            } else if (data.tv_results.length != 0) {
-                return data.tv_results[0].poster_path
-            } else if (data.tv_episode_results.length != 0) {
-                return data.tv_episode_results[0].poster_path
-            } else if (data.tv_season_results.length != 0) {
-                return data.tv_season_results[0].poster_path
-            } else {
-                return ''
+/**
+ * Fonction asynchrone qui récupère l'URL de l'affiche d'un film à partir de son identifiant IMDb en utilisant l'API The Movie Database (TMDb).
+ * @param {string} imdbId - L'identifiant IMDb du film.
+ * @returns {Promise<string>} - Une promesse qui résout avec l'URL de l'affiche ou est rejetée avec une erreur si l'affiche n'est pas trouvée.
+ */
+function getPictures(imdbId) {
+    return fetch(
+        `https://api.themoviedb.org/3/find/${imdbId}?external_source=imdb_id&api_key=${API_KEY}`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            for (i in data) {
+                if (data[i][0] && data[i][0].poster_path) {
+                    return `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${data[i][0].poster_path}`;
+                }
             }
-
-        })
-        .catch(error => console.log(error))
-
-    return `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datas}`
+            throw new Error("Poster Not Found");
+        });
 }
 
-let cartFilmData = {};
-const cartFilmDisplay = async () => {
+/**
+ * Fonction asynchrone qui met à jour les informations d'un film côté serveur en effectuant une requête POST.
+ * @param {string} cartFilm - L'identifiant du film à mettre à jour.
+ * @param {Object} updatedFilmData - Les données mises à jour du film au format JSON.
+ * @returns {Promise<string>} - Une promesse qui résout avec la réponse du serveur ou est rejetée avec une erreur en cas d'échec.
+ */
+async function updateFilmOnServer(cartFilm, updatedFilmData) {
     try {
-        cartFilmData = await getFilmById(cartFilm);
-        if (cartFilmData && typeof cartFilmData === 'object') {
-            const picture = await getPictures(cartFilmData.referenceNumber);
-            cartFilmData.picture = picture;
-            const roles = await getRoleByFilm();
-            cartFilmData.role = roles;
-            console.log(cartFilmData);
+        const response = await fetch(`${urlAppFilm}${cartFilm}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(updatedFilmData)
+        });
 
-            document.getElementById("affich-film").innerHTML = `
-                <h2 class="text-start">Les données d'un film</h2>
-                <article class="col-12 col-md-4" id=${cartFilmData.id}>
-                    <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
-                        <img class="card-img-top" src=${cartFilmData.picture} alt="image du film ${cartFilmData.title}" />
-                        <div class="card-body">
-                            <h5 class="card-title text-truncate">${cartFilmData.title}</h5>
-                            <p class="card-text text-truncate">${cartFilmData.genres.join(", ")}</p>
-                        </div>
-                    </div>
-                </article>
-                <div class="col-12 col-md-8">
-                    <h5>Sommaire</h5>
-                    <p class="summary">${cartFilmData.summary}</p>
-                    <h5>Réalisateur(s)</h5>
-                    <p>${cartFilmData.realisators.join(", ")}</p>
-                    <h5>Langue</h5>
-                    <p>${cartFilmData.language}</p>
-                    <h5>Année de sortie</h5>
-                    <p>${cartFilmData.yearEnd}</p>
-                </div>
-                <div>
-                    <h5 class="text-center">Acteurs et rôles</h5>
-                    <ol class="list-group list-group-numbered">
-                    ${roles.map(role => `<li class="list-group-item">actor: '${role.actor}', role: '${role.role}'</li>`).join('')}
-                    </ol>
-                </div>
-            `;
+        if (!response.ok) {
+            throw new Error(`Erreur lors de la mise à jour du film (${response.status})`);
+        }
+
+        const data = await response.text();
+        return data;
+    } catch (error) {
+        console.error('Erreur lors de la mise à jour du film:', error);
+        throw error;
+    }
+}
+
+// Fonction pour obtenir et formater les données du film
+async function fetchAndFormatFilmData(cartFilm) {
+    try {
+        const filmData = await getFilmById(cartFilm);
+
+        if (filmData && typeof filmData === 'object') {
+            const picture = await getPictures(filmData.referenceNumber)
+            .then((picture) => picture)
+            .catch((e) => "images/no-poster-available.jpg");
+            filmData.picture = picture;
+            const roles = await getRoleByFilm();
+            filmData.role = roles;
+            return filmData;
         } else {
-            console.error('Invalid data format received from getFilmById.');
+            console.error('Format de données non valide reçu de getFilmById.');
+            return null;
         }
     } catch (error) {
-        console.error('Error displaying films:', error);
+        console.error('Erreur de récupération ou de formatage des données film :', error);
+        return null;
     }
-};
+}
+
+// Fonction pour afficher les données du film
+function displayFilm(filmData) {
+    if (!filmData) {
+        return;
+    }
+
+    document.getElementById("affich-film").innerHTML = `
+        <!-- Votre code d'affichage du film ici -->
+        <div class="d-flex justify-content-between bg-white affichFilm gy-2">
+        <h2 class="text-start">${filmData.title}</h2>
+        <a href="#formulaire" id="modifier" class="modifFilm">Modifier <i class="fa-regular fa-pen-to-square"></i></a>
+    </div>
+    <article class="col-12 col-md-4" id=${filmData.id}>
+        <div class="card shadow p-3 mb-3 bg-body-tertiary rounded">
+            <img class="card-img-top" src=${filmData.picture} alt="image du film ${filmData.title}" />
+            <div class="card-body">
+                <h5 class="card-title text-truncate">${filmData.title}</h5>
+                <p class="card-text text-truncate">${filmData.genres.join(", ")}</p>
+                <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="${filmData.rating*10}" aria-valuemin="0" aria-valuemax="100">
+                <div class="progress-bar progress-bar-striped" style="width: ${filmData.rating*10}%">${filmData.rating*10} %</div>
+            </div>
+        </div>
+    </article>
+    <div class="col-12 col-md-6">
+        <h5> <i class="fa-solid fa-book-open"></i> Sommaire</h5>
+        <p class="summary">${filmData.summary}</p>
+        <h5>Réalisateur(s)</h5>
+        <p>${filmData.realisators.map(real => real.fullName).join(", ")}</p>
+        <h5>Pays</h5>
+        <p>${filmData.country}</p>
+        <h5><i class="fa-solid fa-language"></i> Langue</h5>
+        <p>${filmData.language}</p>
+        <h5>Année de sortie</h5>
+        <p>${filmData.year}</p>    
+    </div>
+    <div>
+        <h5 class="text-center">Acteurs et rôles</h5>
+        <table class="table table-bordered table-hover">
+            <thead>
+               <tr>
+                  <th scope="col">Acteurs</th>
+                  <th scope="col">Rôles</th>
+               </tr>
+            </thead>
+        ${filmData.role.map(role => `
+            <tbody>
+                <tr>
+                    <td>${role.actor}</td>
+                    <td>${role.role}</td>
+                </tr>
+            </tbody>`).join("")}
+        </table>
+    </div>
+    <div id="formulaire" style="display: none;">
+    <h5 class="formulaire" >Formulaire de mise à jour</h5>
+    <form id="updateFilmForm" class="row g-3">  
+    <!-- Champs de formulaire pour la modification -->
+        <div class="col-md-6">
+            <label for="title" class="form-label">Titre :</label>
+            <input type="text" id="title" class="form-control" name="title" value="${filmData.title}">
+        </div>
+        <div class="col-md-6">
+            <label for="sommaire" class="form-label">Sommaire :</label>
+            <input type="text" id="sommaire" class="form-control" name="sommaire" value="${filmData.summary}">
+        </div>
+        <div class="col-md-6">
+            <label for="notation" class="form-label">Notation :</label>
+            <input type="text" id="notation" class="form-control" name="notation" value="${filmData.rating}">
+        </div>
+        <div class="col-md-6">
+            <label for="annee" class="form-label">Année de sortie :</label>
+            <input type="text" id="annee" class="form-control" name="annee" value="${filmData.year}">
+        </div>
+        <div class="col-12">
+            <button type="submit" class="btn btn-outline-dark">Mettre à jour</button>
+        </div>
+    </form>
+    <div id="message"></div>
+    </div>
+    `;
+    // Récupération des éléments DOM pour le lien de modification et le formulaire
+    const modifier = document.getElementById("modifier");
+    const formulaire = document.getElementById("formulaire");
+    // Ajout d'un gestionnaire d'événements au clic sur le lien de modification
+    modifier.addEventListener("click", function (event) {
+        event.preventDefault();
+        // Rend le formulaire visible en modifiant le style de l'élément
+        formulaire.style.display = "block";
+        // Fait défiler la page jusqu'au début du formulaire avec une animation fluide
+        formulaire.scrollIntoView({
+            behavior: "smooth",
+            block: "start",
+        });
+    });
+    // Récupération de l'élément DOM pour le formulaire de mise à jour et le conteneur de message
+    let updateFilmForm = document.getElementById("updateFilmForm");
+    const messageContainer = document.getElementById('message');
+
+    updateFilmForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+        // Récupération des valeurs des champs du formulaire
+        const updatedFilmData = {
+            title: document.getElementById("title").value,
+            summary: document.getElementById("sommaire").value,
+            rating: document.getElementById("notation").value,
+            year: document.getElementById("annee").value
+        }
+
+        try {
+            // Appel de la fonction de mise à jour du film côté serveur
+            const responseData = await updateFilmOnServer(cartFilm, updatedFilmData);
+            // Affichage du message de succès dans le conteneur
+            console.log('Réponse de mise à jour du film :', responseData);
+            messageContainer.textContent = 'Le film a été mis à jour avec succès.';
+        } catch (error) {
+            // En cas d'erreur, affichage du message d'erreur dans le conteneur
+            console.error('Erreur lors de la mise à jour du film :', error);
+            messageContainer.textContent = 'Erreur lors de la mise à jour du film. Veuillez réessayer.';
+        }
+    });
+}
+
+const footer = document.getElementById("footer");
+function showFooter(){
+    footer.style.display = "block";
+}
+// Ajouter un écouteur d'événements lorsque la page est complètement chargée
+window.addEventListener("load", function () {
+    // Définir un délai de 2 secondes
+    const delaiEnSecondes = 2;
+    // Afficher navPagination après le délai spécifié
+    setTimeout(showFooter,delaiEnSecondes* 1000 ); // Convertir les secondes en millisecondes
+});
+
+// Fonction principale pour afficher le film
+async function cartFilmDisplay() {
+    const filmData = await fetchAndFormatFilmData(cartFilm);
+    console.log(filmData);
+    displayFilm(filmData);
+}
 
 cartFilmDisplay();
--- films.js
+++ films.js
@@ -1,11 +1,17 @@
 
-const urlAppFilms = 'http://localhost:8080/films/all';
+const urlAppFilms = 'http://localhost:8080/films';
 const API_KEY = '8c876ad71559ac44edf7af86b9d77927';
+
+let GLOBAL_SEARCH = true;
 
 const pageSize = 12;
 let totalPageCount = 332; // Nombre total de pages
 let currentPage = 0;
-
+/**
+ * Fonction asynchrone pour récupérer une liste de films depuis une API avec pagination.
+ * @param {number} pageNumber - Le numéro de la page à récupérer.
+ * @returns {Promise} - Une promesse résolue avec les données des films ou rejetée en cas d'erreur.
+ */
 async function getFilms(pageNumber) {
     try {
         const response = await fetch(`${urlAppFilms}?page=${pageNumber}&size=${pageSize}`);
@@ -17,51 +23,68 @@ async function getFilms(pageNumber) {
     }
 }
 
-async function getPictures(imdbId) {
-    const imdbUrl = `https://api.themoviedb.org/3/find/${imdbId}?external_source=imdb_id&api_key=${API_KEY}`;
-    let datas = await fetch(imdbUrl)
-        .then(response => response.json())
-        .then(data => {
-            if (data.movie_results.length != 0) {
-                return data.movie_results[0].poster_path
-            } else if (data.person_results.length != 0) {
-                return data.person_results[0].poster_path
-            } else if (data.tv_results.length != 0) {
-                return data.tv_results[0].poster_path
-            } else if (data.tv_episode_results.length != 0) {
-                return data.tv_episode_results[0].poster_path
-            } else if (data.tv_season_results.length != 0) {
-                return data.tv_season_results[0].poster_path
-            } else {
-                return ''
+async function getFilmsByPeriod(pageNumber, startYear, endYear) {
+    const url = `http://localhost:8080/films/period/year?start=${startYear}&end=${endYear}&page=${pageNumber}&size=${pageSize}`;
+    try {
+        const response = await fetch(`${url}`);
+        const data = await response.json();
+        return data;
+    } catch (error) {
+        console.log('Error fetching filmsByPeriod:', error);
+        throw error;
+    }
+}
+/**
+ * Fonction asynchrone pour récupérer le chemin du poster d'une entité à partir de l'ID IMDb.
+ * @param {string} imdbId - L'ID IMDb de l'entité.
+ * @returns {Promise<string>} - Une promesse résolue avec le chemin du poster complet ou une chaîne vide si aucun poster n'est trouvé.
+ */
+
+
+function getPictures(imdbId) {
+    return fetch(
+        `https://api.themoviedb.org/3/find/${imdbId}?external_source=imdb_id&api_key=${API_KEY}`
+    )
+        .then((response) => response.json())
+        .then((data) => {
+            for (i in data) {
+                if (data[i][0] && data[i][0].poster_path) {
+                    return `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${data[i][0].poster_path}`;
+                }
             }
-
-
-        })
-        .catch(error => console.log(error))
-
-    return `https://www.themoviedb.org/t/p/w300_and_h450_bestv2/${datas}`
+            throw new Error("Poster Not Found");
+        });
 }
 
-async function filmDisplay(pageNumber) {
-    try {
-        if (pageNumber < 0 || pageNumber > totalPageCount) {
-            return; // Ne rien faire si la page demandée est en dehors des limites
-        }
 
-        const filmData = await getFilms(pageNumber);
+// Fonction pour mettre à jour le stockage local avec la valeur actuelle de currentPage
+function updateCurrentPageInLocalStorage(pageNumber) {
+    localStorage.setItem('currentPage', pageNumber);
+}
+
+// Fonction pour obtenir la valeur actuelle de currentPage depuis le stockage local
+function getCurrentPageFromLocalStorage() {
+    return parseInt(localStorage.getItem('currentPage')) || 0;
+}
+
+async function filmDisplay(pageNumber, filmData) {
+    try {
         for (const film of filmData.content) {
-            const picture = await getPictures(film.referenceNumber);
+            const picture = await getPictures(film.referenceNumber)
+                .then((picture) => picture)
+                .catch((e) => "images/no-poster-available.jpg");
             film.picture = picture;
         }
-console.log(filmData);
         document.querySelector(".films").innerHTML = filmData.content.map((film) => `
             <article class="col">
-                <div class="card shadow p-3 mb-5 bg-body-tertiary rounded">
+                <div class="card shadow p-3 mb-3 bg-body-tertiary rounded">
                     <img class="card-img-top" src=${film.picture} alt="image du film ${film.title}" />
                     <div class="card-body">
                         <h5 class="card-title text-truncate">${film.title}</h5>
                         <p class="card-text text-truncate">${film.genres.join(", ")}</p>
+                        <div class="progress" role="progressbar" aria-label="Example with label" aria-valuenow="${film.rating * 10}" aria-valuemin="0" aria-valuemax="100">
+                        <div class="progress-bar progress-bar-striped" style="width: ${film.rating * 10}%">${film.rating * 10} %</div>
+                </div>
                     </div>
                     <div class="card-footer">
                         <button class="btn btn-outline-dark detail-film" id="${film.id}">Voir</button>
@@ -69,17 +92,24 @@ console.log(filmData);
                 </div>
             </article>
         `).join("");
+
+        document.querySelector("#pagination-counter").innerHTML=`
+        <li class="page-item enabled"><a class="page-link">${filmData.pageable.pageNumber+1} / ${filmData.totalPages}</a></li>
+        `
+        document.querySelector("#films-size").innerHTML=`
+        <p class="font-weight-bold">Résultat : ${filmData.totalElements} films</p>
+        `
+        totalPageCount = filmData.totalPages
         let boutons = document.querySelectorAll(".detail-film");
-        console.log(boutons);
-        // Attacher un écouteur d'événements à chaque bouton
+        // Attache un écouteur d'événements à chaque bouton
         boutons.forEach((bouton) => {
-            bouton.addEventListener("click", () =>{
-                console.log(bouton);
+            bouton.addEventListener("click", () => {
                 // Redirection vers la page 'cart-film.html' avec l'ID du bouton en tant que paramètre dans la chaîne de requête
                 window.location = `cart-film.html?${bouton.id}`
             })
         })
         currentPage = pageNumber;
+        updateCurrentPageInLocalStorage(currentPage); // Met à jour le stockage local
         return filmData;
     } catch (error) {
         console.error('Error displaying films:', error);
@@ -87,9 +117,82 @@ console.log(filmData);
     }
 }
 
-document.getElementById("prev").addEventListener("click", () => filmDisplay(currentPage - 1));
-document.getElementById("next").addEventListener("click", () => filmDisplay(currentPage + 1));
+// Récupérez la valeur de currentPage depuis le stockage local lors du chargement de la page
+currentPage = getCurrentPageFromLocalStorage();
 
-// Initial display
-filmDisplay(currentPage);
+const navPagination = document.getElementById("navPagination");
+function showNavPagination() {
+    navPagination.style.display = "block";
+}
 
+const footer = document.getElementById("footer");
+function showFooter(){
+    footer.style.display = "block";
+}
+// Ajouter un écouteur d'événements lorsque la page est complètement chargée
+window.addEventListener("load", function () {
+    // Définir un délai de 2 secondes
+    const delaiEnSecondes = 2;
+    // Afficher navPagination après le délai spécifié
+    setTimeout(showNavPagination, delaiEnSecondes * 1000); // Convertir les secondes en millisecondes
+    this.setTimeout(showFooter,delaiEnSecondes* 1000 );
+});
+
+
+async function getPeriodicDatas(pageNumber) {
+
+    start = parseInt(localStorage.getItem('start'))
+    end = parseInt(localStorage.getItem('end'))
+    const filmPData = await getFilmsByPeriod(pageNumber, start, end);
+    filmDisplay(pageNumber, filmPData);
+}
+
+async function getGlobalData(pageNumber) {
+    if (pageNumber < 0 || pageNumber > totalPageCount) {
+        return; // Ne rien faire si la page demandée est en dehors des limites
+    }
+    const filmData = await getFilms(pageNumber);
+    filmDisplay(pageNumber, filmData);
+}
+
+// Récupération de l'élément DOM pour le formulaire de mise à jour et le conteneur de message
+let updateFilmForm = document.getElementById("searchForm");
+updateFilmForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    GLOBAL_SEARCH = false;
+    // Récupération des valeurs des champs du formulaire
+    const start = document.getElementById("startYear").value;
+    const end = document.getElementById("endYear").value;
+    localStorage.setItem('start', start);
+    localStorage.setItem('end', end);
+    getPeriodicDatas(0);
+});
+
+console.log("cureent page : ", currentPage);
+getGlobalData(currentPage);
+
+document.getElementById("prev").addEventListener("click", () =>{
+    GLOBAL_SEARCH ? getGlobalData(currentPage - 1) : getPeriodicDatas(currentPage - 1)
+    document.getElementById('pageHeader').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+document.getElementById("next").addEventListener("click", () =>{ 
+    GLOBAL_SEARCH ? getGlobalData(currentPage + 1) : getPeriodicDatas(currentPage + 1)
+    document.getElementById('pageHeader').scrollIntoView({
+        behavior: 'smooth'
+    });
+});
+
+
+document.querySelector("#button-filter").addEventListener("click", () => {
+    document.querySelector("#searchForm").hidden = false
+    document.querySelector("#button-filter").hidden = true
+});
+
+document.querySelector("#button-annuler").addEventListener("click", () => {
+    document.querySelector("#searchForm").hidden = true
+    document.querySelector("#button-filter").hidden = false
+    GLOBAL_SEARCH=true
+    getGlobalData(0);
+});
